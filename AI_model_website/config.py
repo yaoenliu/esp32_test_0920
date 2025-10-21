592131af92cfd2a3fa6b4d780edbc114b4cbde14
yaoenliu <peterliu1060@gmail.com>
2025-10-21 11:44:39 +0800

    Extracting settings into a config.ini file

---
+++ AI_model_website/config.py
@@ -0,0 +1,13 @@
+import configparser
+
+
+global MQTT_HOST, MQTT_PORT, server_host, server_port
+
+config = configparser.ConfigParser()
+config.read('config.ini',encoding='utf-8')
+
+MQTT_HOST = config.get('Mqtt', 'broker')
+MQTT_PORT = config.getint('Mqtt', 'port')
+
+server_host = config.get('Server', 'host')
+server_port = config.getint('Server', 'port')
